d33ea4015209a1dcd4120ff91d4374e909c8722f
Savidya Godamune <savi.godamune@gmail.com>
2026-05-17 17:28:30 +0530

    2 deployment issue fix

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 const Login = () => {
     const [isRegister, setIsRegister] = useState(false);
